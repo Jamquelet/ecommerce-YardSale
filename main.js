69a74369eea23b2d25339d2378a448181491e876
Jose Maya <jamquelet@hotmail.com>
2023-01-31 19:04:15 -0500

    array and render products with loop for_of

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ const menuCarritoIcon = document.querySelector(".navbar-shopping-cart");
 const menuHamIcon = document.querySelector(".menu");
 const mobileMenu = document.querySelector(".mobile-menu");
 const aside = document.querySelector('.product-detail'); 
+const cardsContainer = document.querySelector('.cards-container');
 
 navbarEmail.addEventListener("click", toggleDesktopMenu);
 menuHamIcon.addEventListener("click", toggleMobileMenu);
@@ -51,4 +52,72 @@ function toggleCartAside() { //si le damos click cerramos el menuMobile
     }
 } 
 
+const productList = [];
+productList.push({
+    name: 'Bike',
+    price: 120,
+    image: 'https://images.pexels.com/photos/2393835/pexels-photo-2393835.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+});
+productList.push({
+    name: 'Bike Torn',
+    price: 340,
+    image: "https://images.pexels.com/photos/2116475/pexels-photo-2116475.jpeg?auto=compress&cs=tinysrgb&w=600",
+});
+productList.push({
+    name: 'Nake',
+    price: 140,
+    image: "https://images.pexels.com/photos/2116475/pexels-photo-2116475.jpeg?auto=compress&cs=tinysrgb&w=600",
+});
+/*
+    <div class="product-card">
+        <img src="https://images.pexels.com/photos/2393835/pexels-photo-2393835.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="">
+        <div class="product-info">
+            <div>
+            <p>$120,00</p>
+            <p>Bike</p>
+            </div>
+        <figure>
+            <img src="./Assets/icons/bt_add_to_cart.svg" alt="">
+        </figure>
+        </div>
+    </div>
+*/
+function renderProducts(arr) {
+    //recorrer array
+for(product of arr) {
+    const productCard = document.createElement('div');
+    productCard.classList.add('product-card');
 
+    // product={name, price,image} --> product.image
+    const productImg = document.createElement('img');
+    productImg.setAttribute('src', product.image);
+
+    const productInfo = document.createElement('div');
+    productInfo.classList.add('product-info');
+
+    const productInfoDiv = document.createElement('div');
+
+    const productPrice = document.createElement('p');
+    productPrice.innerText = '$' + product.price;
+    const productName = document.createElement('p');
+    productName.innerText = product.name;
+
+    productInfoDiv.appendChild(productPrice);
+    productInfoDiv.appendChild(productName);
+
+    const productInfoFigure = document.createElement('figure');
+    const productImgCart = document.createElement('img');
+    productImgCart.setAttribute('src', './Assets/icons/bt_add_to_cart.svg');
+
+    productInfoFigure.appendChild(productImgCart);
+
+    productInfo.appendChild(productInfoDiv);
+    productInfo.appendChild(productInfoFigure);
+
+    productCard.appendChild(productImg);
+    productCard.appendChild(productInfo);
+
+    cardsContainer.appendChild(productCard);
+}
+}
+renderProducts(productList);//le digo cual es el array con el cual quiero trabajar
